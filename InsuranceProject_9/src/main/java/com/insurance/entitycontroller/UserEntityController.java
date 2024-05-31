14bc4fc813ae968343ff41a00214eb3e2a37e158
unknown <maralakshada14@gmail.com>
2024-05-31 13:45:00 +0530

    Get Spring 2 task added

--- InsuranceProject_9/src/main/java/com/insurance/entitycontroller/UserEntityController.java
+++ InsuranceProject_9/src/main/java/com/insurance/entitycontroller/UserEntityController.java
@@ -2,6 +2,8 @@ package com.insurance.entitycontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.insurance.entity.UserEntity;
 import com.insurance.entityservice.UserEntityService;
+import com.insurance.model.User;
 
 @RestController
 @RequestMapping("/userentity")
@@ -22,4 +25,10 @@ public class UserEntityController {
 		UserEntity entity=userEntityService.saveUserEntity(userEntity);
 		return ResponseEntity.ok().body(entity);
 	}
+
+	@GetMapping("/readuser/{id}")
+	public ResponseEntity<UserEntity> readUserEntity(@PathVariable long id) {
+		UserEntity user = userEntityService.readUserEntity(id);
+		return ResponseEntity.ok().body(user);
+}
 }
